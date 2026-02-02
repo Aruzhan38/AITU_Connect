3b1f8218b5eb1400978182673269eabd8ef59453
Aruzhan38 <241372@astanait.edu.kz>
2026-02-02 12:51:48 +0500

    Aru

--- internal/usecase/canteen.go
+++ internal/usecase/canteen.go
@@ -8,35 +8,25 @@ import (
 )
 
 type CanteenUsecase struct {
-	canteenRepo pkg.CanteenRepository
-	newsRepo    pkg.CanteenNewsRepository
+	canteenRepo *pkg.CanteenRepository
+	newsRepo    *pkg.CanteenNewsRepository
 }
 
-func NewCanteenUsecase(
-	canteenRepo pkg.CanteenRepository,
-	newsRepo pkg.CanteenNewsRepository,
-) *CanteenUsecase {
-	return &CanteenUsecase{
-		canteenRepo: canteenRepo,
-		newsRepo:    newsRepo,
-	}
+func NewCanteenUsecase(canteenRepo *pkg.CanteenRepository, newsRepo *pkg.CanteenNewsRepository) *CanteenUsecase {
+	return &CanteenUsecase{canteenRepo: canteenRepo, newsRepo: newsRepo}
 }
-
 func (u *CanteenUsecase) GetCanteens(ctx context.Context) ([]model.Canteen, error) {
 	return u.canteenRepo.GetAll(ctx)
 }
 
-func (u *CanteenUsecase) CreateNews(ctx context.Context, news model.CanteenNews) (int64, error) {
-	return u.newsRepo.Create(ctx, news)
+func (u *CanteenUsecase) CreateNews(ctx context.Context, n model.CanteenNews) (int64, error) {
+	return u.newsRepo.Create(ctx, n)
 }
 
-func (u *CanteenUsecase) GetNewsByCanteen(
-	ctx context.Context,
-	canteenID string,
-) ([]model.CanteenNews, error) {
+func (u *CanteenUsecase) GetNewsByCanteen(ctx context.Context, canteenID string) ([]model.CanteenNews, error) {
 	return u.newsRepo.GetByCanteen(ctx, canteenID)
 }
 
-func (u *CanteenUsecase) DeleteNews(ctx context.Context, newsID int64) error {
-	return u.newsRepo.Delete(ctx, newsID)
+func (u *CanteenUsecase) DeleteNews(ctx context.Context, id int64) error {
+	return u.newsRepo.Delete(ctx, id)
 }
